1b64ab0d99586fac1ca113b5ee9e4d4e0cdd4378
maxie <mackyjones@gmail.com>
2020-05-19 12:25:57 +0200

    change server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const favicon = require('express-favicon')
 const path = require('path')
 const rp = require('request-promise')
 const serveStatic = require('serve-static')
 
 const app = express()
+
 const port = process.env.PORT || '4000'
 const apiUrl = 'https://api.stocktwits.com/api/2/streams/symbol/'
 
+app.use(favicon(__dirname + '/build/favicon.ico'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 
@@ -32,10 +35,11 @@ app.post('/api', (req, res) => {
 })
 
 // Serve static files from build folder
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  //app.use(express.static(path.join(__dirname, '../build')))
-  app.use(serveStatic(path.join(__dirname, 'build')))
-}
+//app.use(express.static(path.join(__dirname, '../build')))
+app.use(serveStatic(__dirname))
+app.use(serveStatic(path.join(__dirname, 'build')))
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(port, () => console.log(`Server listening http://localhost:${port}`))
