c87b92c49ef368a5504041638d4532e432c4efd7
maxie <mackyjones@gmail.com>
2020-05-19 11:53:54 +0200

    rebuild

--- server/heroku.js
+++ server/heroku.js
@@ -34,15 +34,6 @@ app.post('/api', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, '../build')))
-
-  // Handle React routing, return all requests to React app
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', '/index.html'), (err) => {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
-  })
 }
 
 app.listen(port, () => console.log(`Server listening http://localhost:${port}`))
